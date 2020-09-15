a3d5286358b9b3debab2bad0a0f64b68360176fa
Adrian Melo <adrianmelo1390995@gmail.com>
2020-09-15 05:16:17 -0500

    added allowed Hosts

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'c!_zg$mpgk329hrc-ogm-@dq#hm&i-*n7))sm8=g%pnbqcehob'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://safe-taiga-06275.herokuapp.com','http://localhost:8080/#']
 
 
 # Application definition
